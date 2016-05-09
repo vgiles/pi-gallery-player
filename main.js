b0b66ee3d51756db00a2f966abf35f6fddf3a6ac
Vincent Giles <vin@vgiles.net>
2016-05-09 18:30:42 +1000

    Added the general overview of tasks needed to be done, and variables/constants that I think I'll need.

--- main.js
+++ main.js
@@ -11,7 +11,22 @@ app.on('read', function() {
     height: 700,
     resizeable: false,
     width: 700,
+    fullscreen: true, // test this out? No idea if it works...
   });
 
   mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
 });
+
+// variables that I will need
+const audioFileOne = etc; // locate file on disk
+const audioFileTwo = etc; // locate file on disk
+const audioFileThree = etc; // locate file on disk
+
+var artist = 'Vincent Giles';
+var yearOfComp = '2015';
+var compTitle = etc; // get metadata information
+
+// make a looping audio player. if the piece is finished, moved to the next, then the next, then do the whole thing again
+
+
+// meanwhile, display the relevant information in the UI.
