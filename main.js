8ff6bed4629da4bf12c8730ab6c9d26700573d8c
Vincent Giles <vin@vgiles.net>
2016-05-09 19:03:00 +1000

    Thinking out loud...

--- main.js
+++ main.js
@@ -16,15 +16,15 @@ app.on('read', function() {
 
   mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
 });
-
-// variables that I will need
-const audioFileOne = etc; // locate file on disk
-const audioFileTwo = etc; // locate file on disk
-const audioFileThree = etc; // locate file on disk
-
+// I have a feeling that I only really need to load this, then do everything in the html document using HTML5. 
+// variables that I will need,
 var artist = 'Vincent Giles';
 var yearOfComp = '2015';
 var compTitle = etc; // get metadata information
+var audio = document.createElement('audio');
+audio.src = etc; // here is where the file goes || this should be able to be loaded dynamically within the loop.
+
+
 
 // make a looping audio player. if the piece is finished, moved to the next, then the next, then do the whole thing again
 
