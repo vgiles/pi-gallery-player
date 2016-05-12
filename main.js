13c8217e0b9bcc25c24389d5c72c1f843209d151
Vincent Giles <vin@vgiles.net>
2016-05-12 18:10:13 +1000

    Playback and dynamic setting of text working. Just need to see if it works properly. Then set electron.io up.

--- main.js
+++ main.js
@@ -16,17 +16,3 @@ app.on('read', function() {
 
   mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
 });
-// I have a feeling that I only really need to load this, then do everything in the html document using HTML5. 
-// variables that I will need,
-var artist = 'Vincent Giles';
-var yearOfComp = '2015';
-var compTitle = etc; // get metadata information
-var audio = document.createElement('audio');
-audio.src = etc; // here is where the file goes || this should be able to be loaded dynamically within the loop.
-
-
-
-// make a looping audio player. if the piece is finished, moved to the next, then the next, then do the whole thing again
-
-
-// meanwhile, display the relevant information in the UI.
